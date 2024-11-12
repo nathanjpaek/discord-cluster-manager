955fa65ecbe15e88d60cd69d85701397818f5e29
Mark Saroufim <marksaroufim@meta.com>
2024-11-11 16:29:09 -0800

    NVIDA test (#17)
    * NVIDA test
    
    * update
    
    * update

--- train.py
+++ train.py
@@ -1,12 +1,8 @@
 import torch
 
-a = torch.Tensor([1, 2, 3, 4, 5]).to("cuda")
-b= torch.Tensor([1, 2, 3, 4, 5]).to("cuda")
+a = torch.Tensor([1, 2, 3, 4, 5]).cuda()
+b= torch.Tensor([1, 2, 3, 4, 5]).cuda()
 
-if torch.cuda.is_available():
-    gpu_name = torch.cuda.get_device_name(0)
-    print(f"GPU Name: {gpu_name}")
-else:
-    print("No GPU available")
-
+print(a)
+print(b)
 print(a + b)
