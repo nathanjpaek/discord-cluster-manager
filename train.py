8634493cff34c84687fa28017449536f635be7d0
Mark Saroufim <marksaroufim@meta.com>
2024-11-11 16:12:56 -0800

    update

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import torch
 
-a = torch.Tensor([1, 2, 3, 4, 5]).to('cuda')
-b= torch.Tensor([1, 2, 3, 4, 5]).to('cuda')
+a = torch.Tensor([1, 2, 3, 4, 5]).to("cuda")
+b= torch.Tensor([1, 2, 3, 4, 5]).to("cuda")
 
 if torch.cuda.is_available():
     gpu_name = torch.cuda.get_device_name(0)
