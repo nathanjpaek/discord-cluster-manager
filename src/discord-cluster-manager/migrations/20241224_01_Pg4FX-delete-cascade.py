0573c60f4db8aa26b7f9cde64a2723a42eb3ab4b
Ben Horowitz <bhorowit@gmail.com>
2024-12-24 14:53:41 -0800

    changed foreign keys to 'on delete cascade' (#73)
    * changed foreign keys to 'on delete cascade'
    
    * changes to make ruff happy

---
+++ src/discord-cluster-manager/migrations/20241224_01_Pg4FX-delete-cascade.py
@@ -0,0 +1,61 @@
+"""
+This migration changes all existing foreign key constraints to be 'on delete
+cascade'.
+"""
+
+from yoyo import step
+
+__depends__ = {'20241222_01_ELxU5-add-gpu-types'}
+
+steps = [
+    # submission table
+    step(
+        """
+        ALTER TABLE leaderboard.submission
+        DROP CONSTRAINT submission_leaderboard_id_fkey
+        """
+    ),
+    step(
+        """
+        ALTER TABLE leaderboard.submission
+        ADD CONSTRAINT submission_leaderboard_id_fkey
+        FOREIGN KEY (leaderboard_id)
+        REFERENCES leaderboard.leaderboard(id)
+        ON DELETE CASCADE
+        """
+    ),
+
+    # runinfo table
+    step(
+        """
+        ALTER TABLE leaderboard.runinfo
+        DROP CONSTRAINT runinfo_submission_id_fkey
+        """
+    ),
+    step(
+        """
+        ALTER TABLE leaderboard.runinfo
+        ADD CONSTRAINT runinfo_submission_id_fkey
+        FOREIGN KEY (submission_id)
+        REFERENCES leaderboard.submission(id)
+        ON DELETE CASCADE
+        """
+    ),
+
+    # gpu_type table
+    step(
+        """
+        ALTER TABLE leaderboard.gpu_type
+        DROP CONSTRAINT gpu_type_leaderboard_id_fkey
+        """
+    ),
+    step(
+        """
+        ALTER TABLE leaderboard.gpu_type
+        ADD CONSTRAINT gpu_type_leaderboard_id_fkey
+        FOREIGN KEY (leaderboard_id)
+        REFERENCES leaderboard.leaderboard(id)
+        ON DELETE CASCADE
+        """
+    ),
+]
