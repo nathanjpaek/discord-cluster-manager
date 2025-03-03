092c7d6f311e90c83cb116229652e9f028a90917
Charles Frye <cfrye59@gmail.com>
2025-03-03 12:56:35 -0800

    stricter gpu scheduling for a100s and h100s (#201)

--- src/discord-cluster-manager/modal_runner_archs.py
+++ src/discord-cluster-manager/modal_runner_archs.py
@@ -4,11 +4,12 @@
 
 from modal_runner import app, cuda_image, modal_run_config
 
-gpus = ["T4", "L4", "A100", "H100"]
+gpus = ["T4", "L4", "A100-80GB", "H100!"]
 for gpu in gpus:
-    app.function(gpu=gpu, image=cuda_image, name=f"run_cuda_script_{gpu.lower()}", serialized=True)(
+    gpu_slug = gpu.lower().split("-")[0].strip('!')
+    app.function(gpu=gpu, image=cuda_image, name=f"run_cuda_script_{gpu_slug}", serialized=True)(
         modal_run_config
     )
     app.function(
-        gpu=gpu, image=cuda_image, name=f"run_pytorch_script_{gpu.lower()}", serialized=True
+        gpu=gpu, image=cuda_image, name=f"run_pytorch_script_{gpu_slug}", serialized=True
     )(modal_run_config)
